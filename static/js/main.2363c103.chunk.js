0207a3361e6a6cc0a1f935fab7f0b699fb0ae01c
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-11-01 11:48:45 +0700

    Updates

---
+++ static/js/main.2363c103.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact2-goit-react-hw-02-phonebook"]=this["webpackJsonpreact2-goit-react-hw-02-phonebook"]||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),o=c.n(n),r=c(3),a=c.n(r),i=(c(8),c(9),c(0));var s=function(){return Object(i.jsx)("div",{className:"App",children:"888"})};a.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.2363c103.chunk.js.map
